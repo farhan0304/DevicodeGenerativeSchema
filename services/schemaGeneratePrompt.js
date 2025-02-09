989d0fb7e035f6141b580e06f1f85df051e157af
farhan0304 <farhannizam23@gmail.com>
2025-02-09 19:53:13 +0530

    -Fix bug

--- services/schemaGeneratePrompt.js
+++ services/schemaGeneratePrompt.js
@@ -1,11 +1,22 @@
 import {prompt,jsonSchema} from "./prompts.js";
 import { PromptTemplate } from "@langchain/core/prompts";
-import { getModelInstance } from "./modelinstance.js";
+import { ChatFireworks } from "@langchain/community/chat_models/fireworks";
 
 
 const langchainGenerative = async (modelType,schemaName,details) =>{
     
-    const model = getModelInstance(modelType);
+    let model;
+    if(String(modelType) == "llama3.3"){
+        model = new ChatFireworks({
+            model: "accounts/fireworks/models/llama-v3p3-70b-instruct",
+            temperature: 0.5
+        })
+    }else{
+        model = new ChatFireworks({
+            model: "accounts/fireworks/models/llama-v3p2-3b-instruct",
+            temperature: 0.5
+        });
+    }
 
     const schemaTemplate = PromptTemplate.fromTemplate(prompt);
     const schemaPrompt =  await schemaTemplate.invoke({ schemaName,details}); 
